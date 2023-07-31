a2a315da47f1f2c4fd107917b06f5bf3bafd0755
Ricardo Matheus <ricardo.mathper@gmail.com>
2023-07-31 18:47:49 -0500

    Initial commit

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|string|email',
+            'password' => 'required|string|min:6',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'El campo email es requerido',
+            'password.required' => 'El campo contraseña es requerido',
+        ];
+    }
+}
---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'phone_number' => 'required|string',
+            'address' => 'required|string|max:255',
+            'password' => 'required|string|min:6|confirmed',
+        ];
+    }
+}
